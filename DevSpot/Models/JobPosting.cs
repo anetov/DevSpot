8454e4cd80446176c616919018bb323fbb3ebf12
amalanetov <amalanetov@gmail.com>
2025-06-16 10:19:18 +0500

    Add job posting management functionality
    - Updated ApplicationDbContext to include DbSet<JobPosting>.
    - Created migration files for JobPostings table with necessary fields.
    - Updated ApplicationDbContextModelSnapshot to reflect new entity.
    - Registered JobPostingRepository for dependency injection in Program.cs.
    - Introduced JobPosting model class with validation attributes.
    - Added IRepository<T> interface for generic CRUD operations.
    - Implemented JobPostingRepository for managing job postings.

---
+++ DevSpot/Models/JobPosting.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace DevSpot.Models
+{
+    public class JobPosting
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        public DateTime PostedDate { get; set; } = DateTime.Now;
+
+        public bool IsApproved { get; set; }
+
+        // Ссылка на Id пользователя, который создал JobPosting
+        [Required]
+        public string UserId { get; set; }
+
+        // Дает возможность на ссылание на объект пользователя(IdentityUser)
+        // Если не было бы ForeignKey, то пользователя получали бы с помощью query, ссылаясь по Id
+        [ForeignKey(nameof(UserId))]
+        public IdentityUser User { get; set; }
+    }
+}
